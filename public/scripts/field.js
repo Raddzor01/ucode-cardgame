6daf6c002ceec9545a77151b5ff406d3289c6948
hicoolasss <criopsdaun@gmail.com>
2023-10-02 16:53:42 +0300

    merge

--- public/scripts/field.js
+++ public/scripts/field.js
@@ -17,34 +17,34 @@ socket.on('startGame', (data) => {
         console.log(data[0]);
         console.log(data[1]);
         // Получаем имя текущего пользователя
-        const currentUserLogin = data[0].login;  // используем данные из data
-    
+        const currentUserLogin = userData.Login;  // используем данные из data
+
         // Получаем контейнеры игроков
         const firstPlayerContainer = document.getElementById('first-player');
         const secondPlayerContainer = document.getElementById('second-player');
-    
+
         // Проверяем, кто из игроков является текущим пользователем, и заполняем контейнеры соответственно
         if (firstPlayer.login === currentUserLogin) {
-            document.getElementById('first-player-login').textContent = firstPlayer.login;
-            document.getElementById('second-player-login').textContent = secondPlayer.login;  // Добавлено
-            // ваш аватар будет всегда снизу
-            firstPlayerContainer.classList.add('current-user');
-            secondPlayerContainer.classList.remove('current-user');
+                document.getElementById('first-player-login').textContent = firstPlayer.login;
+                document.getElementById('second-player-login').textContent = secondPlayer.login;  // Добавлено
+                // ваш аватар будет всегда снизу
+                firstPlayerContainer.classList.add('current-user');
+                secondPlayerContainer.classList.remove('current-user');
         } else {
-            document.getElementById('first-player-login').textContent = firstPlayer.login;  // Добавлено
-            document.getElementById('second-player-login').textContent = secondPlayer.login;
-            // ваш аватар будет всегда снизу
-            secondPlayerContainer.classList.add('current-user');
-            firstPlayerContainer.classList.remove('current-user');
+                document.getElementById('first-player-login').textContent = firstPlayer.login;  // Добавлено
+                document.getElementById('second-player-login').textContent = secondPlayer.login;
+                // ваш аватар будет всегда снизу
+                secondPlayerContainer.classList.add('current-user');
+                firstPlayerContainer.classList.remove('current-user');
         }
-    
+
         // Заполняем остальные данные первого и второго игроков
         // ...
-    });
-    
-    
-    
-    
+});
+
+
+
+
 
 
 function getCookie(name) {
